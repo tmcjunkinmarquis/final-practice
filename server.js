baf170f54a32f6853d6c43c2168a0efd06683d9e
tmcjunkinmarquis <tmcjunkinmarquis@gmail.com>
2018-09-03 11:05:38 -0600

    Add get endpoint and handler for cute puppies

--- server.js
+++ server.js
@@ -5,17 +5,17 @@ app.set('port', process.env.port || 3000)
 app.locals.title = '** Final Practice **'
 
 app.locals.puppies=[
-  {name: "Yvette", age: 5, color: "white"},
-  {name: "Gaston", age: 5, color: "white with a tan spot"}
+  {id: 1, name: "Yvette", age: 5, color: "white"},
+  {id: 2, name: "Gaston", age: 5, color: "white with a tan spot"}
 ]
 
 app.get('/', (request, response)=> {
   response.send('I am practicing for the final')
 })
 
-app.get('api/v1/cute-pets', (request, response)=>{
-  const cute-pets = app.locals.cute-pets;
-  response.json({puppies})
+app.get('/api/v1/cute-puppies', (request, response)=>{
+  const cutePuppies = app.locals.puppies;
+  response.json({cutePuppies})
 })
 
 app.listen(app.get('port'), ()=>{
