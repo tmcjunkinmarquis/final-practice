ea4892da60ea3a4f1d893735953746a3659a61ea
tmcjunkinmarquis <tmcjunkinmarquis@gmail.com>
2018-09-03 10:25:42 -0600

    Add app.locals.puppies

--- server.js
+++ server.js
@@ -4,9 +4,16 @@ const app = express()
 app.set('port', process.env.port || 3000)
 app.locals.title = '** Final Practice **'
 
+app.locals.puppies=[
+  {name: "Yvette", age: 5, color: "white"},
+  {name: "Gaston", age: 5, color: "white with a tan spot"}
+]
+
 app.get('/', (request, response)=> {
   response.send('I am practicing for the final')
 })
+
+
 
 app.listen(app.get('port'), ()=>{
   console.log(`${app.locals.title} is running on ${app.get('port')}.`);
