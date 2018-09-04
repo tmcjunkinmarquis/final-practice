3d4862dfcde7b190f0d661220b6c0775ce437e2b
tmcjunkinmarquis <tmcjunkinmarquis@gmail.com>
2018-09-03 19:49:54 -0600

    Add test environment to knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -1,7 +1,16 @@
 // Update with your config settings.
 
 module.exports = {
-
+  test: {
+    client: 'pg',
+    connection: 'postgres://localhost/finalpracticeone_test',
+    migrations: {
+      directory: '/db/migrations'
+    },
+    seeds: {
+      directory: '/db/seeds/test'
+    }
+  },
   development: {
     client: 'pg',
     connection: 'postgres://localhost/finalpracticeone',
